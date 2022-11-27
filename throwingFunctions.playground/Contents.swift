bf4a6633a00ecc24bd7b88069cf6ce7cabe2254a
coder-hello2019 <67556078+coder-hello2019@users.noreply.github.com>
2022-11-27 21:56:00 +0000

    add detail on optional trys to throwing functions playground

--- throwingFunctions.playground/Contents.swift
+++ throwingFunctions.playground/Contents.swift
@@ -2,15 +2,12 @@ import Cocoa
 
 /* Fundamentals
  
-Throwing functions and do-catch functionality are basic used to handle situations in which a function may return an actual result to us but it may also end up throwing an error. The example here is self-contained but this is particularly useful when e.g. making network calls as these are often out of our control as developers.
+Throwing functions and do-catch functionality are used to handle situations in which a function may throw an error. The example here is self-contained but this is particularly useful, for example, when making network calls.
  
  The key logic here works as follows:
-    * whenever we think that a function may return an error, we have to 'try' and get the result, using a try statement;
+    * whenever we think that a function may return an error, we have to first 'try' and get the actual result from it, using a 'try' statement;
     * try statements are enclosed in the 'do' portion of do-catch statements - we try to get the result inside the 'do', and catch any errors inside the 'catch';
     * in order to be able to 'catch' an error, we need to use a throwing function i.e. a function capable of throwing an error. These functions are marked with a 'throws' keyword before their return type. This keyword tells us that a function may throw an error (but it doesn't necessarily have to).
- 
- Resources:
-    *
  
  */
 
@@ -19,16 +16,25 @@ enum DummyError: Error {
     case testError
 }
 
+// MARK: demo throwing functions
+
 // a dummy throwing function - it returns a string if 'true' is passed into it and throws an error otherwise
 func thisFunctionThrows (isTrue: Bool) throws -> String {
     if isTrue == true {
-        return "True :)"
+        return "This should print :)"
     } else {
         throw DummyError.testError
     }
 }
 
-// this is how we would use the throwing function in practice - in the most basic way possible
+// a dummy throwing function that ALWAYS throws an error
+func thisFuncAlwaysThrows () throws -> Void {
+    throw DummyError.testError
+}
+
+// MARK: demo do-catch statements
+
+// this is how we would use a throwing function in practice - in the most basic way possible
 do {
     var testVar = try thisFunctionThrows(isTrue: true)
     print("Success, we've received the following string back from the function: \(testVar)")
@@ -36,10 +42,39 @@ do {
     print("The function returned an error")
 }
 
-// we can make this a bit more advanced by giving the error a var name in the catch statement and printing some more info about the error
-
+// we can make this a bit more helpful by giving the error a var name in the catch statement and printing some info about it
 do {
     let testVar = try thisFunctionThrows(isTrue: false)
 } catch let errorText {
     print("There was an error: \(errorText)")
 }
+
+// One useful point to note is that you can have as many 'try' statements in a 'do' as you want. HOWEVER, if any one of them does throw an error, you're immediately dropped into the 'catch' (even if a later one of the 'try's would have produced an actual result).
+// A way to get around this is to make the first try inside the 'do' optional (i.e. try?). This way, firstTry will just have a nil value instead of throwing an error and, because an error is not thrown, we won't be pushed into the catch. See more on optional 'try's below.
+
+do {
+    let firstTry: Void = try thisFuncAlwaysThrows()
+    let secondTry = try thisFunctionThrows(isTrue: true)
+} catch {
+    print("We get thrown here even though secondTry would have produced a String without any errors")
+}
+
+/* Optional try statements
+ 
+ In some situations, we may not care about the error being thrown (but think about this carefully!). In that case, instead of throwing and catching the error, we can make the try statement optional ('try' becomes 'try?'). Instead of throwing the error, the 'try' interprets the errror as a nil value and proceeds.
+ 
+ Optional 'try's can be force unwrapped but, as per usual, this is generally not a good thing to attempt.
+ 
+ */
+
+do {
+    if let firstTry = try? thisFuncAlwaysThrows() {
+        print("This should never be printed because the try always receives an error")
+    }
+    // secondTry will now print
+    let secondTry = try thisFunctionThrows(isTrue: true)
+    print(secondTry)
+} catch {
+    print("We don't get thrown here, unless something goes wrong with secondTry")
+}
+
